67053ed3a0484c7b50b1a39c57e599472b64e675
macsoftechnologies <macsoftechnologies@gmail.com>
2025-09-11 02:33:59 +0530

    update on 11-09-2025

--- src/app/views/Models/RequestDto.ts
+++ src/app/views/Models/RequestDto.ts
@@ -1,5 +1,6 @@
 
 export class RequestDto {
+    username: string;
     work_type: string;
     pressure_pneumatic: string;
     pressure_hydrostatic: string;
@@ -198,6 +199,7 @@ export class FilesRequestDto {
 }
 
 export class EditRequestDto {
+    fields: string;
     work_type: string;
     pressure_pneumatic: string;
     pressure_hydrostatic: string;
